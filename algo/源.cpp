67d25837cdc21783539354c441e18490a6418d82
叶志晟 <yzs981130@126.com>
2017-04-06 15:56:08 +0800

    n后问题

--- algo/源.cpp
+++ algo/源.cpp
@@ -1,71 +1,39 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
 #include <cstdio>
+#include <cmath>
 #include <vector>
 #include <cstring>
 using namespace std;
-#define MAXN 205
-int graph[MAXN][MAXN];
-int match[MAXN];
-int book[MAXN];
-int n, m, p;
-vector<int>e[MAXN];
-bool dfs(int u)
+#define MAXN 14
+int x[MAXN];
+int n, ans = 0;
+bool check(int p)
 {
-	for (int i = 0; i < e[u].size(); i++)
+	for (int i = 1; i < p; i++)
+		if ((abs(x[i] - x[p]) == abs(i - p)) || (x[i] == x[p])) 
+			return false;
+	return true;
+}
+void DFS(int p)
+{
+	if (p > n)
 	{
-		int v = e[u][i];
-		if (!book[v])
-		{
-			book[v] = true;
-			if (match[v] == 0 || dfs(match[v]))
-			{
-				match[v] = u;
-				return true;
-			}
-		}
+		ans++;
+		return;
 	}
-	return false;
+	for (int i = 1; i <= n; i++)
+	{
+		x[p] = i;
+		if (check(p))			
+			DFS(p + 1);
+	}
 }
 int main()
 {
-	int t;
-	scanf("%d", &t);
-	while (t--)
-	{
-		scanf("%d %d", &p, &n);
-		if (p > n)
-		{
-			printf("NO\n");
-			continue;
-		}
-		memset(match, 0, sizeof(match));
-		memset(e, 0, sizeof(e));
-		int s, k;
-		for (int i = 1; i <= p; i++)
-		{
-			scanf("%d", &s);
-			for (int j = 0; j < s; j++)
-			{
-				scanf("%d", &k);
-				e[i].push_back(k);
-			}
-		}
-		bool ans = true;
-		for (int i = 1; i <= p; i++)
-		{
-			memset(book, 0, sizeof(book));
-			if (!dfs(i))
-			{
-				ans = false;
-				break;
-			}
-		}
-		if (ans)
-			printf("YES\n");
-		else
-			printf("NO\n");
-	}
+	cin >> n;
+	DFS(1);
+	cout << ans << endl;
 	system("pause");
 	return 0;
 }
